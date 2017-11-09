def0314281c17c0f69568212b0c373f4ac4b5eaa
Filipe Teixeira <fmcta@iscte-iul.pt>
2017-11-09 15:02:36 +0000

    Testes de Métodos feitos em partiha

---
+++ src/Tester/Tester.java
@@ -0,0 +1,22 @@
+package Tester;
+
+/*
+ * Classe feita para testar funcionalidades do git
+ * @Badjoras
+ * */
+public class Tester {
+	
+	
+	
+	void methodA(){
+		
+		
+		
+	}
+	
+	
+	void methodB(){
+		
+	}
+
+}
